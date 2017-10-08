f3124a82fa2c7d939dabf46adf6b2fb7b2cf0498
Edward Barnard <ewbarnard@embarqmail.com>
2017-10-08 07:57:50 -0500

    Begin command-line importer shell.

---
+++ src/Shell/GarminShell.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ewb
+ * Date: 10/8/17
+ * Time: 7:44 AM
+ */
+
+namespace App\Shell;
+
+use Cake\Console\Shell;
+
+class GarminShell extends Shell {
+
+    /** @noinspection PhpHierarchyChecksInspection */
+    /**
+     * @param array ...$args
+     * @return void
+     */
+    public function main(...$args) {
+        $this->verbose('Beginning import run', 2);
+    }
+}
